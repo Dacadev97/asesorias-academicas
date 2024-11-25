a89807bd8b455242905c57a3c55d5b09af9b127e
Daca.dev <daca.dev97@gmail.com>
2024-11-25 05:22:36 -0500

    add Docker Compose configuration; expose port 3000 and remove proyecto routes

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const tipoProyectoRoutes = require("./src/routes/tipoProyectoRoutes");
 const clienteRoutes = require("./src/routes/clienteRoutes");
 const etapaRoutes = require("./src/routes/etapaRoutes");
 const universidadRoutes = require("./src/routes/universidadRoutes");
-const proyectoRoutes = require("./src/routes/proyectoRoutes");
 
 dotenv.config();
 
@@ -29,7 +28,6 @@ app.use("/api/tipoproyecto", tipoProyectoRoutes);
 app.use("/api/cliente", clienteRoutes);
 app.use("/api/etapa", etapaRoutes);
 app.use("/api/universidad", universidadRoutes);
-app.use("/api/proyecto", proyectoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
